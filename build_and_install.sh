49c4dc8673f1de007c52a5d3e277df0e2fde66ce
Shunsuke Kirino <Shunsuke.Kirino@access-company.com>
2013-06-05 15:59:09 +0900

    Use D 2.062

--- build_and_install.sh
+++ build_and_install.sh
@@ -10,22 +10,23 @@ git submodule init
 git submodule update
 
 # dmd compiler
+# EDIT posix.mak 32 => 64
 cd dmd/src
-cat posix.mak | sed -e s/MODEL=32/MODEL\?=32/ > posix_mod.mak
-MODEL=`getconf LONG_BIT` make -f posix_mod.mak dmd
-sleep 5
-rm posix_mod.mak
+make clean -f posix.mak
+MODEL=`getconf LONG_BIT` make -f posix.mak dmd
 sudo cp dmd ${D_INSTALL_PATH}/bin/
 cd ../..
 
 # druntime
 cd druntime
+make clean -f posix.mak
 MODEL=`getconf LONG_BIT` make -f posix.mak
 sudo cp -r import/* ${D_INSTALL_PATH}/import/
 cd ..
 
 # phobos
 cd phobos
+make clean -f posix.mak
 MODEL=`getconf LONG_BIT` make -f posix.mak
 sudo cp -r std/ ${D_INSTALL_PATH}/import/
 sudo cp generated/**/libphobos2.a ${D_INSTALL_PATH}/lib/
@@ -36,9 +37,10 @@ sudo cp ./dmd.conf ${D_INSTALL_PATH}/bin/
 
 # gtkd
 cd GtkD
+make clean
 make
 sudo cp -r src/* ${D_INSTALL_PATH}/import/
 sudo rm ${D_INSTALL_PATH}/import/dsss.conf
-sudo cp libgtkd.a ${D_INSTALL_PATH}/lib/
+sudo cp libgtkd-2.a ${D_INSTALL_PATH}/lib/
 cd ..
 
