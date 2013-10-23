cf8473acb46bbc0f2a99cd66722d7b4d7f3668b1
Shunsuke Kirino <shunsuke.kirino@gmail.com>
2013-10-24 00:29:39 +0900

    Update

--- build_and_install.sh
+++ build_and_install.sh
@@ -8,36 +8,34 @@ sudo mkdir -p ${D_INSTALL_PATH}/import
 cd ~/code/D/SetupDDevelopmentEnvironment/
 
 # dmd compiler
-cd dmd/src
+cd dmd/
 make clean -f posix.mak
-MODEL=`getconf LONG_BIT` make -f posix.mak dmd
-sudo cp dmd ${D_INSTALL_PATH}/bin/
-cd ../..
+make -f posix.mak
+sudo cp src/dmd ${D_INSTALL_PATH}/bin/
+cd ..
 
 # druntime
 cd druntime
 make clean -f posix.mak
-MODEL=`getconf LONG_BIT` make -f posix.mak
+make -f posix.mak
 sudo cp -r import/* ${D_INSTALL_PATH}/import/
 cd ..
 
 # phobos
 cd phobos
 make clean -f posix.mak
-MODEL=`getconf LONG_BIT` make -f posix.mak
+make -f posix.mak
 sudo cp -r std/ ${D_INSTALL_PATH}/import/
 sudo cp generated/**/libphobos2* ${D_INSTALL_PATH}/lib/
 cd ..
 
 # set import/library paths for dmd compiler
-sudo cp ./dmd.conf ${D_INSTALL_PATH}/bin/
+sudo cp dmd.conf ${D_INSTALL_PATH}/bin/
 
 # gtkd
 cd GtkD
 make clean
-make
 sudo cp -r src/* ${D_INSTALL_PATH}/import/
-sudo rm ${D_INSTALL_PATH}/import/**/*.o
+make
 sudo cp libgtkd-2.a ${D_INSTALL_PATH}/lib/
 cd ..
-
